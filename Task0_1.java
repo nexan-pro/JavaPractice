723f9572211008d2f4976f832be862583477a717
0x000N3X4N <0xN3X4N@protonmail.com>
2018-09-12 22:39:24 +0300

    UPD

--- Task0_1.java
+++ Task0_1.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Task0_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double R, xdst = 5, dx = 0.5;
+        System.out.println("Enter R: ");
+        R = input.nextDouble();
+        System.out.println("------------------------------------------------------------------------------------------");
+        System.out.println("======================Y==================================X================================");
+        System.out.println("------------------------------------------------------------------------------------------");
+        for (double x = -9; x <= xdst; x += dx)
+            System.out.println("\t\t\t\t\t" + func(x, R) + "\t\t\t\t\t" + x);
+    }
+    public static double func(double x, double R){
+        if (x<-5 && x>=-9)
+            return (-Math.sqrt(Math.pow(R,2)-Math.pow(x+7,2))+2);
+        else if (x <=-4 && x>=-5)
+            return (2);
+        else if (x<=0 && x>-4)
+            return (-0.5*x);
+        else if(x<=Math.PI && x>0)
+            return (Math.sin(x));
+        else if (x<=5 && x>=Math.PI)
+            return (x - Math.PI);
+        else return -1; //error
+    }
 }
--- Task0_2.java
+++ Task0_2.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+/*import  java.util.Scanner;
 
 public class Task0_2 {
+    public static void main(String[] args) {
+        double x, y;
+        System.out.println("Enter R: ");
+        Scanner input = new Scanner(System.in);
+        double r = input.nextDouble();
+        boolean flag = false;
+        for (int i = 0; i < 10; ++i) {
+            x = getrandomNumber();
+            y = getrandomNumber();
+            System.out.print("x = " + x + " y = " + y);
+            if (x > -r && x < r && y > -r && y < r) {
+                //System.out.println("popal v kvadrat");
+                flag = true;
+                if (Math.pow((-r - x), 2) + Math.pow(r - y, 2) < Math.pow(r, 2)) {
+                    //System.out.println("promah verh");
+                    flag = false;
+                }
+                if (Math.pow(r - x, 2) + Math.pow(-r - y, 2) < Math.pow(r, 2)) {
+                    //System.out.println("promah nizh");
+                    flag = false;
+                }
+            }
+            if (flag) System.out.println(" popal");
+        }
+    }
+
+    public static double getrandomNumber() {
+        return (Math.random() - Math.random());
+    }
 }
+*/
