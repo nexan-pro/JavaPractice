07db262d85f41d79642909c61ab029c4f14d98fb
0x000N3X4N <0xN3X4N@protonmail.com>
2018-09-18 14:02:09 +0300

    upd

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Xn, Xk, dx, eps: ");
-        double x, sum, n, i, Xn, Xk, dx, eps, rez;
+        double x, sum, n, i, Xn, Xk, dx, eps;
         do {
             Xn = input.nextDouble();
             Xk = input.nextDouble();
@@ -23,13 +23,9 @@ public class main {
                 i++;
                 n++;
             }
-            System.out.println("x: " + Xn + "\t f(x): " + sum + " \t Кол-во членов" + n);
+            System.out.println("x: " + Xn + "\t f(x): " + sum + " \t Кол-во членов: " + n + " \t" + Math.exp(-x));
             Xn += dx;
         }
-       // System.out.println("-----------------------------------------------------------");
-        //System.out.println("|         value          |   ...     |   ....    |   .... |");
-        //System.out.println("-----------------------------------------------------------");
-
     }
 
     private static double getFactorial(double N){
@@ -38,18 +34,7 @@ public class main {
         if (N == 0)
             return 1;
         else
-            return N * getFactorial(N - 1);
-    }
-
-
-    public static double func(double x, double eps) {
-        double s = 1;
-        double el = 1;
-        double n = 0;
-        while (Math.abs(el) > eps) {
-            n++;
-        }
-        return n;
+            return (N * getFactorial(N - 1));
     }
 }
 
