2eacd7d6afab997ec7802367c926618de1c1c2bf
0x000N3X4N <0xN3X4N@protonmail.com>
2018-09-19 07:56:36 +0300

    upd

--- task_3_3_Taylor_sequence.java
+++ task_3_3_Taylor_sequence.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main {
+public class Taylor_sequence_3_3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Xn, Xk, dx, eps: ");
