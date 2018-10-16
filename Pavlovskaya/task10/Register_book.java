603e95618c87e38627e75312994de7c96e469a40
0x000N3X4N <0xN3X4N@protonmail.com>
2018-10-16 14:52:16 +0300

    fixed sorting && added print output students with 2

--- Pavlovskaya/task10/Register_book.java
+++
@@ -1,7 +0,0 @@
-package Pavlovskaya.task10;
-
-public class Register_book {
-    public static void main(String[] args) {
-
-    }
-}
--- Pavlovskaya/task10/main.java
+++ Pavlovskaya/task10/main.java
@@ -1,59 +1,72 @@
 package Pavlovskaya.task10;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class main {
-    static class rer {
+    static class Student {
         String surname, name, patronymic;
         int numberOfGroup;
-        int[] perfomance = new int[5];
+        int[] progress = new int[5];
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int size = 2;
-        String[] SNP = new String[size];
-        rer[] obj = new rer[size];
+        Map bad = new HashMap<String, Integer>();
+        int size = 3;
+        Student[] obj = new Student[size];
         for (int i = 0; i < size; i++)
-            obj[i] = new rer(); //initialize all objects
+            obj[i] = new Student(); //initialize all objects
         for (int i = 0; i < size; ++i){
              System.out.print("Enter initials (surname, name, patronymic): ");
              obj[i].surname = input.nextLine();
              obj[i].name = input.nextLine();
              obj[i].patronymic = input.nextLine();
-             SNP[i] = obj[i].surname + " " + obj[i].name + " " + obj[i].patronymic;
              System.out.println("Enter num of your group: ");
              obj[i].numberOfGroup = input.nextInt();
-             System.out.print("Enter your perfomance (size of array = 5): ");
-             for (int j = 0; j < 5; j++)
-                 obj[i].perfomance[j] = input.nextInt();
+             System.out.print("Enter your progress (size of array = 5): ");
+             for (int j = 0; j < 5; j++) {
+                 obj[i].progress[j] = input.nextInt();
+                 if (obj[i].progress[j] == 2)
+                     bad.put(obj[i].surname, obj[i].numberOfGroup);
+             }
              System.out.println();
              input.nextLine();
         }
-        System.out.println("Modified List: ");
+        System.out.println("~~~~~~~Array without sort: ~~~~~~~~");
         print(obj, size);
-        System.out.print("Sort surname of alphabet: \r\n");
-        sort(SNP);
+        System.out.println("~~~~~~~Modified array: ~~~~~~~");
+        sort(obj, size);
+        print(obj, size);
+        System.out.println("=================================\r\n");
+        System.out.println((bad.isEmpty()) ? "Error, students with 2 not found" : bad);
     }
-    static void print(rer[] obj, int size){
+
+    static void print(Student[] obj, int size){
+        System.out.println("\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         for (int i = 0; i < size; ++i) {
             System.out.print("Surname/Name/Patronymic: " + obj[i].surname + " " + obj[i].name + " " + obj[i].patronymic + "\r\n");
             System.out.print("Number of group: " + obj[i].numberOfGroup + "\r\n");
-            System.out.print("Perfomance: ");
+            System.out.print("progress: ");
             for (int j = 0; j < 5; ++j)
-                System.out.print(" " + obj[i].perfomance[j]);
-            System.out.println();
+                System.out.print(" " + obj[i].progress[j]);
+            System.out.println("\r\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         }
     }
-    static String[] sort(String[] src){
-        for (int i = 0; i < src.length; i++)
-            for (int j = i + 1; j < src.length; j++){
-                if (src[j].compareTo(src[i]) < 0){
-                    String tmp = src[i];
-                    src[i] = src[j];
-                    src[j] = tmp;
+    //case-sensitive bubble sort
+    static Student[] sort(Student[] obj, int size){
+        for (int i = 0; i < size; i++)
+            for (int j = i + 1; j < size; j++){
+                if (obj[j].surname.compareTo(obj[i].surname) < 0){
+                    Student tmp = obj[i];
+                    obj[i] = obj[j];
+                    obj[j] = tmp;
                 }
             }
-        return (src);
+        return (obj);
     }
+    //static void printBad(HashMap<String, Integer> src){
+
+    //}
 }
 
